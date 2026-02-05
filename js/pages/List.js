89d19a20b9ed0330e01d0cd602e3fa042b0a5bc7
Randomfr500 <somrrandomloltrollguy@gmail.com>
2026-02-05 22:03:36 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -118,7 +118,7 @@ export default {
                         Do not use secret routes or bug routes
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        The levels difficulty has to be lower than a easy extreme demon
                     </p>
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
